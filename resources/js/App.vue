9ef8ad3f23acf04c53af66d861fb46ea5955ffe7
YoussefBe <joebelal7@gmail.com>
2021-07-16 17:58:12 +0200

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        a
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log("i love vue");
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+require('./bootstrap');
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
